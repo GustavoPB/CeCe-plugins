a5e9285425a6bf173f640648fde77a33442013a3
Gustavo <g.puig.baeza@gmail.com>
2016-10-23 20:32:52 +0200

    +Nueva definicion de fago

--- cell/DrawablePhage.hpp
+++ cell/DrawablePhage.hpp
@@ -23,51 +23,100 @@
 /*                                                                          */
 /* ************************************************************************ */
 
-// CeCe
-#include "cece/plugin/definition.hpp"
-#include "cece/plugin/Api.hpp"
-#include "cece/plugin/Repository.hpp"
-
-// Plugin
-#include "Ecoli.hpp"
-#include "Yeast.hpp"
-#include "Cell.hpp"
-#include "Phage.hpp"
-#include "StoreMolecules.hpp"
+#pragma once
 
 /* ************************************************************************ */
 
-using namespace cece;
-using namespace cece::plugin::cell;
+// CeCe
+#include "cece/core/Real.hpp"
+#include "cece/core/Units.hpp"
+#include "cece/render/Color.hpp"
+#include "cece/render/Buffer.hpp"
+#include "cece/render/Shader.hpp"
+#include "cece/render/Program.hpp"
+
+/* ************************************************************************ */
+
+namespace cece { namespace render { class Context; } }
+
+/* ************************************************************************ */
+
+namespace cece {
+namespace plugin {
+namespace cell {
 
 /* ************************************************************************ */
 
 /**
- * @brief Cell plugin API.
+ * @brief Rendering object for phage.
  */
-class CellApi : public plugin::Api
+class DrawablePhage
 {
 
-    /**
-     * @brief On plugin load.
-     *
-     * @param repository Plugin repository.
-     */
-    void onLoad(plugin::Repository& repository) const override
-    {
-        repository.registerApi(this).
-            registerObject<Cell>("cell.Cell").
-            registerObject<Yeast>("cell.Yeast").
-			registerObject<Ecoli>("cell.Ecoli").
-			registerObject<Ecoli>("cell.Phage").
-            registerProgram<StoreMolecules>("cell.store-molecules")
-        ;
-    }
+// Public Ctors & Dtors
+public:
 
+
+    /**
+     * @brief Constructor.
+     *
+     * @param context Rendering context.
+     */
+    explicit DrawablePhage(render::Context& context);
+
+
+// Public Operations
+public:
+
+
+    /**
+     * @brief Render at given position with given rotation.
+     *
+     * @param context Render context.
+     * @param size    Phage size.
+     * @param budSize Bud size.
+     * @param color   Phage color.
+     * @param idColor Phage identification color.
+     */
+    void draw(render::Context& context, RealType size, RealType budSize,
+        const render::Color& color, const render::Color& idColor) noexcept;
+
+
+// Private Data Members
+private:
+
+    /// Buffer object.
+    render::Buffer m_buffer;
+
+    /// Vertex shader.
+    render::Shader m_vertexShader;
+
+    /// Fragment shader.
+    render::Shader m_fragmentShader;
+
+    /// Shader program.
+    render::Program m_program;
+
+    /// Phage color (GFP, YFP, RFP).
+    render::Program::UniformId m_uniformColor;
+
+    /// Border color.
+    render::Program::UniformId m_uniformIdColor;
+
+    /// If bud should be drawn.
+    render::Program::UniformId m_uniformHasBud;
+
+    /// Phage size variable.
+    render::Program::UniformId m_uniformSizeMain;
+
+    /// Bud size variable.
+    render::Program::UniformId m_uniformSizeBud;
 };
 
 /* ************************************************************************ */
 
-CECE_DEFINE_PLUGIN(cell, CellApi)
+}
+}
+}
 
 /* ************************************************************************ */
